07acdcf092f542307964caedcec68f5f9b3e0cc4
Albert Kuliński <albert.kulinski@hotmail.com>
2018-11-12 17:28:44 +0100

    Velocity dependencies added

--- src/main/java/com/mrowka/transactionswebapp/core/methods/HelloWordRoutes.java
+++ src/main/java/com/mrowka/transactionswebapp/core/methods/HelloWordRoutes.java
@@ -1,11 +1,19 @@
 package com.mrowka.transactionswebapp.core.methods;
 
+import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloWordRoutes {
 
     public Object helloWord(Request request, Response response){
-        return "Hello Word";
+        Map<String, Object> model = new HashMap<>();
+        return new VelocityTemplateEngine().render(
+                new ModelAndView(model, "index.vm")
+        );
     }
 }
